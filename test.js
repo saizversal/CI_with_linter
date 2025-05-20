edec90ea43a4d1da14a4b24375c1e7b9f55a89a8
saizversal <saikumarzversal@gmail.com>
2025-05-20 11:23:58 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+console.log('Running basic tests...');
+
+// Example test: check if 2+2=4
+if (2 + 2 === 4) {
+  console.log('Test passed!');
+} else {
+  console.error('Test failed!');
+  process.exit(1);
+}
